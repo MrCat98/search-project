e7a2a00f34cea54adce7d774da87826457b71178
MrCat98 <maksampan4@gmail.com>
2026-01-22 22:49:00 +0900

    потключил апи,подключил javaskript и css

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const input = document.getElementById("search");
 const autocomplete = document.getElementById("autocomplete");
 const repoList = document.getElementById("repo-list");
+
+fetch("https://api.github.com/search/topics?q=Q")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("data:", data);
+  });
