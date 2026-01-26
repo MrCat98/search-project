befbf2c07e17246527e4268a542620cb4e696c9f
MrCat98 <maksampan4@gmail.com>
2026-01-26 17:52:26 +0900

    autocomplite , api fixed

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
 const input = document.getElementById("search");
 const autocomplete = document.getElementById("autocomplete");
 const repoList = document.getElementById("repo-list");
+// функция debounce
+const debounce = (fn, delay) => {
+  let timeout;
 
-fetch("https://api.github.com/search/topics?q=Q")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log("data:", data);
+  return function (...arr) {
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      (fn.apply(this, arr), delay);
+    }, delay);
+  };
+};
+
+// получение апи
+
+async function fetchRep(query) {
+  if (!query.trim()) {
+    clearAutocomplete(data.items);
+    return;
+  }
+  try {
+    const response = await fetch(
+      "https://api.github.com/search/repositories?q=Q",
+    );
+    if (response.ok) {
+      const data = await response.json();
+      showAutocomplete(data.items);
+    }
+  } catch (e) {
+    console.error("Ошибка запроса", e);
+  }
+}
+
+// Автокомплит
+
+function showAutocomplete(repos) {
+  clearAutocomplete();
+
+  repos.forEach((element) => {
+    const li = document.createElement("li");
+    li.classList.add("autocomplete-item");
+    li.textContent = element.name;
+    li.addEventListener("click", () => addRepoToList(element));
+    autocompleteList.appendChild(li);
   });
+}
